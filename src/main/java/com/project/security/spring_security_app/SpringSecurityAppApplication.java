f55de658b38c1c6a8456ced696acbafb4f8e3b0a
mishraditi <aditimishra91924@gmail.com>
2025-02-21 20:52:17 +0530

    Creating Spring Boot Project for Spring Security

---
+++ src/main/java/com/project/security/spring_security_app/SpringSecurityAppApplication.java
@@ -0,0 +1,14 @@
+package com.project.security.spring_security_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class SpringSecurityAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAppApplication.class, args);
+	}
+
+}
